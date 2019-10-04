0ff31f768acc5cfe9838fa1d48e6d53da4030822
joeriverhavert <joeri.verhavert.y7236@student.hogent.be>
2019-10-04 21:07:19 +0200

    new versie uploaden

--- provisioning/WIN-DC1.ps1
+++ provisioning/WIN-DC1.ps1
@@ -2,6 +2,12 @@ Clear-Host;
 Import-Module ServerManager;
 Import-Module PackageManagement;
 
+#Controleren updates
+Write-host("Controleren op updates..Even geduld aub.")
+Install-Module PSWindowsUpdate;
+Get-WindowsUpdate;
+Install-WindowsUpdate;
+
 #instellen van toetsenbord,regio en taal.
 Write-host("Het toetsenbord wordt in gesteld op AZERTY");
 Set-WinUserLanguageList -LanguageList NL-BE -Force;
@@ -18,38 +24,19 @@ Rename-NetAdapter -Name "Ethernet 2" -NewName "LAN";
 Write-host("AD DS feature wordt gëinstalleerd.");
 Install-WindowsFeature AD-Domain-Services -IncludeManagementTools;
 
-#promoten server naar domaincontroller.
-Write-host("Server wordt gepromoveerd naar domaincontroller.");
-Install-ADDSForest
--CreateDnsDelegation:$false
--DatabasePath "C:\Windows\NTDS"
--DomainName "joeri.periode1"
--DomainNetbiosName "periode1"
--ForestMode "Win2019R2"
--InstallDns:$true
--LogPath "C:\Windows\NTDS"
--NoRebootOnCompletion:$false
--SysvolPath "C:\Windows\SYSVOL"
--Force:$true
-
 #installeren en configuren van DHCP.
 Write-host("DHCP feature wordt geinstalleerd.");
 Install-WindowsFeature -Name 'DHCP' -IncludeManagementTools;
 
 #Configureren van DNS
-Write-host("Configuren van DNS settings.");
+#Write-host("Configuren van DNS settings.");
 #Set-DnsClientServerAddress -InterfaceAlias 'WAN' -ServerAddresses 10.6.0.2
 
-#Controleren updates
-Write-host("Controleren op updates..Even geduld aub.")
-Install-Module PSWindowsUpdate;
-Get-WindowsUpdate;
-Install-WindowsUpdate;
+#promoten server naar domeincontroller in nieuwe forest.
+Write-host("Server wordt gepromoveerd naar domaincontroller in een nieuwe forest.");
+Install-ADDSForest -DomainName "joeri.periode1" -InstallDns -SafeModeAdministratorPassword (ConvertTo-SecureString -AsPlainText "Joeri1311!" -Force) -Force -Verbose;
 
 #Systeem herstarten
-Write-host("Het systeem zal herstart worden.")
-Restart-Computer -Force
+Write-host("Installatie is compleet,het systeem zal eerst herstart worden.")
 
-#Controle
-#Get-smbshare
-#dcdiag.exe
+
--- provisioning/WIN-DC2.ps1
+++ provisioning/WIN-DC2.ps1
@@ -2,28 +2,32 @@ Clear-Host;
 Import-Module ServerManager;
 Import-Module PackageManagement;
 
+#Controleren updates
+Write-host("Controleren op updates..Even geduld aub.")
+Install-Module PSWindowsUpdate;
+Get-WindowsUpdate;
+Install-WindowsUpdate;
+
 #instellen van toetsenbord,regio en taal.
 Write-host("Het toetsenbord wordt in gesteld op AZERTY");
 Set-WinUserLanguageList -LanguageList NL-BE -Force;
 Write-host("De tijdzone wordt ingesteld op Brussel(Roman Standard Time)");
 Set-TimeZone "Romance Standard Time";
 
+#aanpassen van ethernet adapter
 Write-host("Netwerkadapter Ethernet heeft nieuwe naamgeving,LAN.");
 Rename-NetAdapter -Name "Ethernet 2" -NewName "LAN";
+Write-host("Ip,subnet,default gateway, DNS van LAN adapter wordt aangepast. ")
+New-NetIPAddress –IPAddress 192.168.100.20 -DefaultGateway 192.168.100.10 -PrefixLength 24 -InterfaceIndex (Get-NetAdapter).InterfaceIndex;
+Set-DNSClientServerAddress -–ServerAddresses 192.168.100.10;
 
 #install AD DS.
 Write-host("AD DS feature wordt gëinstalleerd.");
 Install-WindowsFeature AD-Domain-Services -IncludeManagementTools;
 
 #Server wordt gepromoveerd naar domaincontroller in een bestaande forest.
-Install-ADDSDomainController -CreateDnsDelegation:$false -DatabasePath 'C:\Windows\NTDS' -DomainName 'joeri.periode1' 
-	-InstallDns:$false -LogPath 'C:\Windows\NTDS' -NoGlobalCatalog:$false -SiteName 'joeri.periode1' -SysvolPath 'C:\Windows\SYSVOL' -NoRebootOnCompletion -Force
+Install-ADDSDomainController  -DomainName 'joeri.periode1' -SiteName 'joeri.periode1' -NoRebootOnCompletion -Force
 
-#Controleren updates
-Write-host("Controleren op updates..Even geduld aub.")
-Install-Module PSWindowsUpdate;
-Get-WindowsUpdate;
-Install-WindowsUpdate;
 
 #Systeem herstarten
 Write-host("Het systeem zal herstart worden.")
